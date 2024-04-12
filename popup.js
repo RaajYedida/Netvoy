45b289e4af372b96ba866e39d74356f062b03d14
Raaj Yedida <166455374+RaajYedida@users.noreply.github.com>
2024-04-12 18:42:51 -0500

    Barebones V 0.0.2
    Refined the UI

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Display visit times
     chrome.storage.local.get('visitTimes', function(data) {
         const timesDiv = document.getElementById('times');
         if (data.visitTimes) {
             Object.keys(data.visitTimes).forEach(function(url) {
                 const ms = data.visitTimes[url];
-                const minutes = Math.floor(ms / 60000);
-                const seconds = ((ms % 60000) / 1000).toFixed(0);
-                const timeFormatted = `${minutes}m ${seconds}s`;
+                const hours = Math.floor(ms / 3600000); // Convert milliseconds to hours
+                const minutes = Math.floor((ms % 3600000) / 60000); // Convert remaining milliseconds to minutes
+                const seconds = Math.floor((ms % 60000) / 1000); // Convert remaining milliseconds to seconds
+                const timeFormatted = `${hours} Hrs ${minutes} Mins ${seconds} Secs`;
                 const timeElement = document.createElement('div');
-                timeElement.textContent = `${url}: ${timeFormatted}`;
+                timeElement.classList.add('time-entry');
+
+                // Remove "www." from the URL
+                const siteName = url.replace(/^www\./,'');
+
+                // Fetch the favicon for the site
+                const faviconUrl = `https://www.google.com/s2/favicons?domain=${url}`;
+                const favicon = document.createElement('img');
+                favicon.src = faviconUrl;
+                favicon.classList.add('site-logo');
+
+                timeElement.innerHTML = `<div class="time-entry-title">${siteName}</div><div class="time">${timeFormatted}</div>`;
+                timeElement.prepend(favicon);
                 timesDiv.appendChild(timeElement);
             });
         } else {
