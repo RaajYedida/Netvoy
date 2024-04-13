ab8c223b69d3a8b941b77b72b3e3c5488c3114e5
Raaj Yedida <166455374+RaajYedida@users.noreply.github.com>
2024-04-12 19:25:04 -0500

    minor changes

--- popup.js
+++ popup.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Display visit times
-    chrome.storage.local.get('visitTimes', function(data) {
+    chrome.storage.local.get('visitTimes', async function(data) {
         const timesDiv = document.getElementById('times');
         if (data.visitTimes) {
-            Object.keys(data.visitTimes).forEach(function(url) {
-                const ms = data.visitTimes[url];
+            // Sort visit times in descending order based on time spent
+            const sortedVisitTimes = Object.entries(data.visitTimes).sort((a, b) => b[1] - a[1]);
+            
+            // Display a maximum of five sites
+            const displayCount = Math.min(sortedVisitTimes.length, 5);
+            for (let i = 0; i < displayCount; i++) {
+                const [url, ms] = sortedVisitTimes[i];
                 const hours = Math.floor(ms / 3600000); // Convert milliseconds to hours
                 const minutes = Math.floor((ms % 3600000) / 60000); // Convert remaining milliseconds to minutes
                 const seconds = Math.floor((ms % 60000) / 1000); // Convert remaining milliseconds to seconds
@@ -24,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 timeElement.innerHTML = `<div class="time-entry-title">${siteName}</div><div class="time">${timeFormatted}</div>`;
                 timeElement.prepend(favicon);
                 timesDiv.appendChild(timeElement);
-            });
+            }
         } else {
             timesDiv.textContent = 'No data available.';
         }
